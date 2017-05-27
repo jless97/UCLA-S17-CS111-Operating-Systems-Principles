d85734995ea0c1ce48cb9aa04e3e5b5b5de682a8
Jason Less <JasonLess@s-169-232-79-191.resnet.ucla.edu>
2017-05-27 04:15:17 -0700

    Added implementation for inode summary (5th csv). Need to check loop condition: num_inode or block_size??

--- Project_3a/lab3a.c
+++ Project_3a/lab3a.c
@@ -3,7 +3,7 @@
 ////////////////////////////////////////////////////////////////////////////
 // NAME: Jason Less & Yun Xu
 // EMAIL: jaless1997@gmail.com &
-// ID: 404-640-158 &
+// ID: 404-640-158 & 304-635-157
 // Project 3a: lab3a.c
 
 ////////////////////////////////////////////////////////////////////////////
@@ -16,6 +16,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <math.h>
+#include <time.h>
 
 ////////////////////////////////////////////////////////////////////////////
 /////////////////////////////// Defines ////////////////////////////////////
@@ -30,6 +31,28 @@
 #define DESCRIPTOR_TABLE_SIZE 32
 // Block group descriptor table buffer size
 #define DESCRIPTOR_TABLE_BUFFER_SIZE 32
+// Size of inode
+#define INODE_SIZE 128
+// Inode table buffer size
+#define INODE_BUFFER_SIZE 128
+
+// File type specifiers
+#define EXT2_S_IFREG 32768
+#define EXT2_S_IFDIR 16384
+#define EXT2_S_IFLNK 40960
+// Mode type specifiers
+#define EXT2_S_IRUSR 256
+#define EXT2_S_IWUSR 128
+#define EXT2_S_IXUSR 64
+#define EXT2_S_IRGRP 32
+#define EXT2_S_IWGRP 16
+#define EXT2_S_IXGRP 8
+#define EXT2_S_IROTH 4
+#define EXT2_S_IWOTH 2
+#define EXT2_S_IXOTH 1
+
+// Time field length
+#define TIME_BUFFER 18
 
 ////////////////////////////////////////////////////////////////////////////
 /////////////////////////////// Globals ////////////////////////////////////
@@ -41,8 +64,12 @@ int image_fd;
 char super_block_buf[SUPER_BLOCK_BUFFER_SIZE];
 // Buffer to acquire information for the block group descriptor table
 char descriptor_table_buf[DESCRIPTOR_TABLE_BUFFER_SIZE];
+// Buffer to acquire information for the inodes
+char inode_buf[INODE_BUFFER_SIZE];
 // Number of block groups
 int num_groups;
+// Number of inodes per group
+int num_inodes;
 
 /* Flags */
 
@@ -65,9 +92,12 @@ struct p3_block_group {
 };
 // Block group array
 struct p3_block_group *block_group;
+// Inode table information array
+struct ext2_inode **inode_table;
 
 /* Arrays */
-
+// Array to keep of which inodes are allocated/free for each group
+int **inode_array;
 
 ////////////////////////////////////////////////////////////////////////////
 /////////////////////////////// Prototypes /////////////////////////////////
@@ -81,7 +111,7 @@ void getSuperBlock(struct ext2_super_block *block);
 // Print super block csv record
 void printSuperBlockCSVRecord(void);
 // Acquire block group information
-void getBlockGroup(struct p3_block_group *group);
+void getBlockGroup(void);
 // Print block group csv record
 void printBlockGroupCSVRecord(void);
 // Acquire free block entries
@@ -89,15 +119,13 @@ void getFreeBlock(void);
 // Print free block entires csv record
 void printFreeBlockCSVRecord(int num_block);
 // Acquire free inode entries
-void getFreeInode(void);
+void getFreeInode();
 // Print free inode entrires csv record
 void printFreeInodeCSVRecord(int num_block);
-
 // Acquire inode summary
-
+void getInodeSummary(void);
 // Print inode summary csv record
 void printInodeSummaryCSVRecord(void);
-
 // Acquire directory entries
 
 // Print directory entries csv record
@@ -177,7 +205,7 @@ printSuperBlockCSVRecord(void) {
 /////////////////////////////////// Block Group ////////////////////////////
 ////////////////////////////////////////////////////////////////////////////
 void
-getBlockGroup(struct p3_block_group *group) {
+getBlockGroup(void) {
     // Get the number of block groups
     num_groups = (int)ceil((double) super_block.s_blocks_count / (double) super_block.s_blocks_per_group);
     
@@ -200,33 +228,33 @@ getBlockGroup(struct p3_block_group *group) {
         }
         
         // Group ID number
-        group[i].group_id = i;
+        block_group[i].group_id = i;
         
         // Total number of blocks (if there are leftover blocks, last group has the leftover)
         if ((i = last_group) && (leftover_blocks == 0)) {
-            group[i].g_blocks_count = super_block.s_blocks_per_group;
+            block_group[i].g_blocks_count = super_block.s_blocks_per_group;
         }
         else {
-            group[i].g_blocks_count = leftover_blocks;
+            block_group[i].g_blocks_count = leftover_blocks;
         }
         
         // Total number of inodes
-        group[i].g_inodes_count = super_block.s_inodes_per_group;
+        block_group[i].g_inodes_count = super_block.s_inodes_per_group;
         
         // Total number of free blocks
-        memcpy(&group[i].g_free_blocks_count, descriptor_table_buf, 4);
+        memcpy(&block_group[i].g_free_blocks_count, descriptor_table_buf, 4);
         
         // Total number of free inodes
-        memcpy(&group[i].g_free_inodes_count, descriptor_table_buf + 4, 4);
+        memcpy(&block_group[i].g_free_inodes_count, descriptor_table_buf + 4, 4);
         
         // Block number of free block bitmap
-        memcpy(&group[i].g_block_bitmap, descriptor_table_buf + 8, 2);
+        memcpy(&block_group[i].g_block_bitmap, descriptor_table_buf + 8, 2);
         
         // Block number of free inode bitmap
-        memcpy(&group[i].g_inode_bitmap, descriptor_table_buf + 12, 2);
+        memcpy(&block_group[i].g_inode_bitmap, descriptor_table_buf + 12, 2);
         
         // Block number of first block of inodes
-        memcpy(&group[i].g_inode_table, descriptor_table_buf + 14, 4);
+        memcpy(&block_group[i].g_inode_table, descriptor_table_buf + 14, 4);
     }
 }
 
@@ -295,14 +323,22 @@ printFreeBlockCSVRecord(int num_block) {
 ////////////////////////////////////////////////////////////////////////////
 void
 getFreeInode(void) {
+    int i, j, bit_size = 8, block_size = super_block.s_log_block_size, num_block = 0, bitmask = 1;
+    
+    // Initialize the inode free/allocated array
+    num_inodes = super_block.s_inodes_per_group;
+    inode_array = (int **) malloc(sizeof(int *) * num_groups);
+    for (i = 0; i < num_groups; i++) {
+        inode_array[i] = (int *) malloc(sizeof(int) * num_inodes);
+    }
+    
     // Variable to hold the bitmap block (reading 1 byte at a time)
     uint8_t inode_bitmap_buf;
     
     ssize_t nread;
-    int i, j, bit_size = 8, block_size = super_block.s_log_block_size, num_block = 0, bitmask = 1;
     for (i = 0; i < num_groups; i++) {
         j = 0;
-        while (j != block_size) {
+        while (j != num_inodes) {
             inode_bitmap_buf = 0;
             nread = pread(image_fd, &inode_bitmap_buf, 1, block_group[i].g_inode_bitmap + j);
             if (nread < 0) {
@@ -319,7 +355,14 @@ getFreeInode(void) {
             while (bit_size != 0) {
                 // If the bit being checked is a 0, then the corresponding block is free
                 if ((inode_bitmap_buf && bitmask) == 0) {
+                    // Set portion of inode array to note that inode block is free
+                    inode_array[i][j] = 0;
+                    
                     printFreeInodeCSVRecord(num_block);
+                }
+                // Set portion of inode array to note that inode block is allocated
+                else {
+                    inode_array[i][j] = 1;
                 }
                 // Shift the bits to the right by 1 to read next bit
                 inode_bitmap_buf = inode_bitmap_buf >> 1;
@@ -344,11 +387,165 @@ printFreeInodeCSVRecord(int num_block) {
 ////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////// Inode Summary ////////////////////////////
 ////////////////////////////////////////////////////////////////////////////
+void
+getInodeSummary(void) {
+    int i, j, k, block_size = super_block.s_log_block_size;
+    
+    // Number of inodes per group
+    num_inodes = super_block.s_inodes_per_group;
+    
+    // Create inode table array
+    inode_table = (struct ext2_inode **) malloc(sizeof(struct ext2_inode *) * num_groups);
+    for (i = 0; i < num_groups; i++) {
+        inode_table[i] = (struct ext2_inode *) malloc(sizeof(struct ext2_inode) * num_inodes);
+    }
+    
+    ssize_t nread;
+    for (i = 0; i < num_groups; i++) {
+        for (j = 0; j < num_inodes; j++) {
+            // Check to see if inode is free/allocated
+            if (inode_array[i][j] == 0) {
+                continue;
+            }
+            else if (inode_array[i][j] == 1) {
+                // For each group, read in a single inode at a time
+                nread = pread(image_fd, &inode_buf, INODE_BUFFER_SIZE, block_group[i].g_inode_table + (j * INODE_SIZE));
+                if (nread < 0) {
+                    fprintf(stderr, "Error reading free inode bitmap info from image file.\n");
+                    exit(EXIT_FAILURE);
+                }
+                
+                // Inode ID number (to be analyzed in printInodeSummaryCSVRecord)
+                // Stored in inode_array
+                
+                // File type and mode (to be analyzed in printInodeSummaryCSVRecord)
+                memcpy(&inode_table[i][j].i_mode, inode_buf, 2);
+                
+                // Owner
+                memcpy(&inode_table[i][j].i_uid, inode_buf + 2, 2);
+                
+                // Group
+                memcpy(&inode_table[i][j].i_gid, inode_buf + 24, 2);
+                
+                // Link count
+                memcpy(&inode_table[i][j].i_links_count, inode_buf + 26, 2);
+                
+                // Creation time (to be analyzed in printInodeSummaryCSVRecord)
+                memcpy(&inode_table[i][j].i_ctime, inode_buf + 12, 4);
+                
+                // Modification time (to be analyzed in printInodeSummaryCSVRecord)
+                memcpy(&inode_table[i][j].i_mtime, inode_buf + 16, 4);
+                
+                // Last access time (to be analyzed in printInodeSummaryCSVRecord)
+                memcpy(&inode_table[i][j].i_atime, inode_buf + 8, 4);
+                
+                // File size
+                memcpy(&inode_table[i][j].i_size, inode_buf + 4, 4);
+                memcpy(&inode_table[i][j].i_dir_acl, inode_buf + 108, 4);
+                
+                // Number of blocks
+                memcpy(&inode_table[i][j].i_blocks, inode_buf + 28, 4);
+                
+                // Direct blocks
+                // Indirect block
+                // Doubly indirect block
+                // Triply indirect block
+                for (k = 0; k < EXT2_N_BLOCKS; k++) {
+                    memcpy(&inode_table[i][j].i_block[k], inode_buf + 40 + (k * 4), 4);
+                }
+            }
+        }
+    }
+}
 
 void
 printInodeSummaryCSVRecord(void) {
     // Print to STDOUT inode summary CSV record
-    
+    int i, j, k, mode, bitmask;
+    long file_size;
+    char file_type;
+    time_t create_time, mod_time, access_time;
+    struct tm *create_gtime, *mod_gtime, *access_gtime;
+    char ctime[TIME_BUFFER], mtime[TIME_BUFFER], atime[TIME_BUFFER];
+    for (i = 0; i < num_groups; i++) {
+        for (j = 0; j < num_inodes; j++) {
+            // Check to see if inode is free/allocated
+            // If free, skip
+            if (inode_array[i][j] == 0) {
+                continue;
+            }
+            // If allocated, print
+            else if (inode_array[i][j] == 1) {
+                // Get file type
+                bitmask = 0xF000;
+                mode = inode_table[i][j].i_mode & bitmask;
+                if (mode == EXT2_S_IFREG) {
+                    file_type = 'f';
+                }
+                else if (mode == EXT2_S_IFDIR) {
+                    file_type = 'd';
+                }
+                else if (mode == EXT2_S_IFLNK) {
+                    file_type = 's';
+                }
+                else {
+                    file_type = '?';
+                }
+                
+                // Get file permissions mode
+                bitmask = 0x0FFF;
+                mode = mode & bitmask;
+                
+                // Get creation time
+                create_time = inode_table[i][j].i_ctime;
+                create_gtime = gmtime(&create_time);
+                if (create_gtime->tm_year > 100 && create_gtime->tm_year < 1000) {
+                    create_gtime->tm_year -= 100;
+                }
+                else if (create_gtime->tm_year > 1000) {
+                    create_gtime->tm_year -= 1000;
+                }
+                create_gtime->tm_mon += 1;
+                snprintf(ctime, 18, "%02d/%02d/%02d %02d:%02d:%02d", create_gtime->tm_mon, create_gtime->tm_mday, create_gtime->tm_year, create_gtime->tm_hour, create_gtime->tm_min, create_gtime->tm_sec);
+                
+                // Get modification time
+                mod_time = inode_table[i][j].i_mtime;
+                mod_gtime = gmtime(&mod_time);
+                if (mod_gtime->tm_year > 100 && mod_gtime->tm_year < 1000) {
+                    mod_gtime->tm_year -= 100;
+                }
+                else if (mod_gtime->tm_year > 1000) {
+                    mod_gtime->tm_year -= 1000;
+                }
+                mod_gtime->tm_mon += 1;
+                snprintf(mtime, 18, "%02d/%02d/%02d %02d:%02d:%02d", create_gtime->tm_mon, create_gtime->tm_mday, create_gtime->tm_year, create_gtime->tm_hour, create_gtime->tm_min, create_gtime->tm_sec);
+                
+                // Get last access time
+                access_time = inode_table[i][j].i_atime;
+                access_gtime = gmtime(&access_time);
+                if (access_gtime->tm_year > 100 && access_gtime->tm_year < 1000) {
+                    access_gtime->tm_year -= 100;
+                }
+                else if (access_gtime->tm_year > 1000) {
+                    access_gtime->tm_year -= 1000;
+                }
+                access_gtime->tm_mon += 1;
+                snprintf(atime, 18, "%02d/%02d/%02d %02d:%02d:%02d", create_gtime->tm_mon, create_gtime->tm_mday, create_gtime->tm_year, create_gtime->tm_hour, create_gtime->tm_min, create_gtime->tm_sec);
+                
+                // File size
+                file_size = (inode_table[i][j].i_dir_acl << 32) | inode_table[i][j].i_size;
+                
+                // Print CSV record
+                fprintf(stdout, "%s,%d,%c,%o,%d,%d,%d,%s,%s,%s,%ld,%d", "INODE", inode_array[i][j], file_type, mode, inode_table[i][j].i_uid, inode_table[i][j].i_gid, inode_table[i][j].i_links_count, ctime, mtime, atime, file_size, inode_table[i][j].i_blocks);
+                for (k = 0; k < EXT2_N_BLOCKS; k++) {
+                    fprintf(stdout, ",%d", inode_table[i][j].i_block[k]);
+                    if (k == (EXT2_N_BLOCKS - 1)) {
+                        fprintf(stdout, "\n");
+                    }
+                }
+            }
+        }
+    }
 }
 
 ////////////////////////////////////////////////////////////////////////////
@@ -385,8 +582,19 @@ main (int argc, char *argv[])
     printSuperBlockCSVRecord();
     
     // Get block group information and print it to STDOUT
-    getBlockGroup(block_group);
+    getBlockGroup();
     printBlockGroupCSVRecord();
+    
+    // Get free block information and print it to STDOUT (handled in getFreeBlock())
+    getFreeBlock();
+    
+    // Get free inode information and print it to STDOUT
+    getFreeInode();
+    printInodeSummaryCSVRecord();
+    
+    // Get inode summary information and print it to STDOUT
+    getInodeSummary();
+    printInodeSummaryCSVRecord();
     
     // If success
     exit(EXIT_SUCCESS);
