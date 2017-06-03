1278e2e74a879fab9ae605d7a33e3afc147bac42
yunx96@gmail.com <yunx96@gmail.com>
2017-06-03 01:04:35 -0700

    main?

--- Project_3b/lab3b.py
+++ Project_3b/lab3b.py
@@ -1,2 +1,15 @@
+#!/usr/bin/python
+
 import sys
-import os
+
+def main():
+	if len(sys.argv < 2) :
+		sys.stderr.write("Error. Usage: ./lab3b [csv file]");
+		sys.stderr.flush();
+		exit(1);
+	else :
+		filename = sys.argv[1];
+
+	csvFile = open(filename, "r");
+
+	
